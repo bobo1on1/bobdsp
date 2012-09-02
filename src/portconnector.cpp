1f479d117df84ff04451615653212e5d02ce62d4
Bob van Loosen <bob.loosen@gmail.com>
2012-09-02 19:53:08 +0200

    fixed: compile warning

--- src/portconnector.cpp
+++ src/portconnector.cpp
@@ -157,6 +157,7 @@ bool CPortConnector::ConnectionsFromJSON(const std::string& json)
     success = ConnectionsFromXML(connections->ToElement());
 
   bool loadfailed = false;
+  (void)loadfailed; //no warnings
   LOADBOOLELEMENT(root, save, OPTIONAL, false, POSTCHECK_NONE);
   if (success && save_p)
     m_bobdsp.SaveConnectionsToFile(ConnectionsToXML());
